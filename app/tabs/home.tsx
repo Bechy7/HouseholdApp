6b9b034f946f2c780a07587156132e68c2da6624
Lucka Smržová <luckasmrzova@Lucka--MacBook-Pro.local>
2025-09-30 15:43:42 +0200

    recipe page chnage, style add, remopve style from home

--- app/tabs/home.tsx
+++ app/tabs/home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../../style/style"
 import { Button, StyleSheet, Text, View } from "react-native";
 import { auth } from "../../firebaseConfig";
 
@@ -10,19 +11,10 @@ export default function HomePage() {
     return (
         <View style={styles.container}>
             <Button title="Logout" onPress={handleLogout} />
-
             <Text style={styles.header}>Home</Text>
             <Text style={styles.text}>Welcome to Luciepoo and Svenjapoo's Household App. Hope it will bring you comfort and joy to your household!</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: { flex: 1, padding: 20, backgroundColor: "#fff" },
-    header: { fontSize: 24, fontWeight: "bold", marginBottom: 20 },
-    text: { fontSize: 16, marginBottom: 10 },
-    inputRow: { flexDirection: "row", marginBottom: 20 },
-    input: { flex: 1, borderWidth: 1, borderColor: "#ccc", borderRadius: 8, padding: 10, marginRight: 10 },
-    noteRow: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingVertical: 5 },
-    note: { fontSize: 18 },
-});
+
--- app/tabs/recipes.tsx
+++ app/tabs/recipes.tsx
@@ -1,2 +1,53 @@
+import { Ionicons } from "@expo/vector-icons";
+import { addDoc, collection } from "firebase/firestore";
+import React, { useState } from "react";
+import { Modal, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { auth, db } from "../../firebaseConfig";
+import styles from "../../style/style";
+
+
 export default function RecipesPage() {
+    const [modalVisible, setModalVisible] = useState(false);
+    const [newRecipe, setNewRecipe] = useState("");
+
+    const addRecipe = async () => {
+        const user = auth.currentUser;
+        console.log(newRecipe.trim());
+        if (!user || !newRecipe.trim()) return;
+        console.log("here");
+        await addDoc(collection(db, "recipes"), {
+            title: newRecipe.trim(),
+        });
+
+        setNewRecipe("");
+    };
+    return (
+        <View style={styles.container}>
+            <View style={styles.one_row}>
+                <Text style={styles.header}>Recipes</Text>
+                <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}><Ionicons name="add" size={24} /></TouchableOpacity>
+            </View>
+            <Modal style={styles.modal}
+                visible={modalVisible}
+                transparent={true}
+                animationType="slide"
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={styles.modal_container}>
+                    <TouchableOpacity style={styles.closeButton} onPress={() => setModalVisible(false)}><Ionicons name="close" size={24} /></TouchableOpacity>
+                    <TextInput
+                        placeholder="Enter recipe name"
+                        onChangeText={setNewRecipe}
+                        style={{ borderWidth: 1, borderColor: '#ccc', padding: 10, marginVertical: 10 }} />
+                    <TouchableOpacity style={styles.addButton} onPress={addRecipe}><Text>Save recipe</Text></TouchableOpacity>
+                </View>
+
+            </Modal>
+
+        </View>
+
+
+    )
 }
+
+
